3a31126e9aeae3895e62eb8b89e27ade8740abaf
Matheus Ibrahim <mibrahimfm@github.com>
2019-09-12 13:27:56 +0000

    Reading integer values from the files

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,27 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using std::string;
+using std::stringstream;
 using std::ifstream;
 using std::ofstream;
 using std::cerr;
+using std::cin;
 using std::cout;
+using std::endl;
+using std::getline;
 
 int main(int argc, char** argv){
     string fileName = argv[1];
 
-    ifstream input;
-    input.open(fileName);
+    ifstream input (fileName);
 
-    int x;
+    int qtParticipants, qtRelations, qtInstructions;
+    int* ages;
+    string reader;
 
-    if (!input) {
-        cerr << "Unable to open file " << fileName;
-        exit(1);   // call system to stop
+    if (input.is_open()) {
+        input >> qtParticipants >> qtRelations >> qtInstructions;
+
+        ages = new int[qtParticipants];
+
+        for(int i = 0; i < qtParticipants; i++){
+            input >> ages[i];
+            cout << ages[i] << " ";
+        }
+
+        int relations[qtRelations][2];
+
+        for(int j = 0; j < qtRelations; j++){
+            input >> relations[j][0] >> relations[j][1];
+            cout << relations[j][0] << " " << relations[j][1] << endl;
+        }
+    }
+    else{
+        cout << "Erro ao abrir " << fileName << endl;
     }
 
-    input >> x;
-
-    cout << x;
 }
