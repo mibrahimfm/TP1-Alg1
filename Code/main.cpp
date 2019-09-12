d5ab3113ff178fb3a06e3b598126a9cd12a507ef
Matheus Ibrahim <matheusibrahimfm@gmail.com>
2019-09-12 11:45:20 -0300

    Documenting code and adding logic to read instructions from file

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,47 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "graph.hpp"
 
 using std::string;
 using std::ifstream;
 using std::ofstream;
-using std::cin;
 using std::cout;
 using std::endl;
 using std::getline;
 
 int main(int argc, char** argv){
+    //Recebe o nome do arquivo passado como parâmetro para o programa
     string fileName = argv[1];
 
+    //Abre o arquivo com uma variável ifstream
     ifstream input (fileName);
 
-    int qtParticipants, qtRelations, qtInstructions;
+    //Declarando variáveis para armazenar os valores lidos no arquivo
+    int qtParticipants, qtRelations, qtInstructions, swap1, swap2, ageCom;
     int* ages;
     int** relations;
-    string reader;
+    char func;
 
+    //Verifica se o arquivo está aberto para ler as entradas
     if (input.is_open()) {
         input >> qtParticipants >> qtRelations >> qtInstructions;
 
-        ages = new int[qtParticipants];
-
-        for(int i = 0; i < qtParticipants; i++){
-            input >> ages[i];
-            cout << ages[i] << " ";
-        }
-
+        //Definindo arrays para armazenar temporariamente as idades e as relações de comando
+        ages = new int[qtParticipants]; 
         relations = new int*[qtRelations];
         for(int i = 0; i < qtRelations; i++){
             relations[i] = new int[2];
         }
 
-        for(int j = 0; j < qtRelations; j++){
+        //Os dois laços a seguir leem as idades e as relações de comando, respectivamente
+        for(int i = 0; i < qtParticipants; i++)
+            input >> ages[i];
+        for(int j = 0; j < qtRelations; j++)
             input >> relations[j][0] >> relations[j][1];
-            cout << relations[j][0] << " " << relations[j][1] << endl;
+
+        for(int k = 0; k < qtInstructions; k++){
+            input >> func;
+            if(func == 'S'){
+                input >> swap1 >> swap2;
+                graph::swap(swap1, swap2);
+            }
+            else if(func == 'C'){
+                input >> ageCom;
+                graph::youngestCommander(ageCom);
+            }
+            else{
+                graph::meeting();
+            }
         }
     }
-    else{
+    else
         cout << "Erro ao abrir " << fileName << endl;
-    }
 
 }
