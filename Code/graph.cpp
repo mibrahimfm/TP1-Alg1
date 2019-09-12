aafdb0fb413b3cf75a5fb13e3f67a04b898bdfeb
Matheus Ibrahim <mibrahimfm@github.com>
2019-09-12 14:51:06 +0000

    Testing the logic of reading instructions

--- Code/graph.cpp
+++ Code/graph.cpp
@@ -1,13 +1,13 @@
 #include "graph.hpp"
 
 void graph::swap(int, int){
-    std::cout << "Swap Succ";
+    std::cout << "Swap Succ" << std::endl;
 }
 
 void graph::youngestCommander(int){
-    std::cout << "Comm Succ";
+    std::cout << "Comm Succ" << std::endl;
 }
 
 void graph::meeting(){
-    std::cout << "Meet Succ";
+    std::cout << "Meet Succ" << std::endl;
 }
