cf4d70f6173887dae72103d0daddeeae7c421be0
Matheus Ibrahim <matheusibrahimfm@gmail.com>
2019-09-12 11:47:11 -0300

    Crating simplified implementations for the functions to test logic of reading from the file

---
+++ Code/graph.cpp
@@ -0,0 +1,13 @@
+#include "graph.hpp"
+
+void graph::swap(int, int){
+    std::cout << "Swap Succ";
+}
+
+void graph::youngestCommander(int){
+    std::cout << "Comm Succ";
+}
+
+void graph::meeting(){
+    std::cout << "Meet Succ";
+}
