8b9cc4e490fb8458df23c96c1eb445d94aee3b05
Matheus Ibrahim <mibrahimfm@github.com>
2019-09-12 14:20:55 +0000

    Defining initial contract of the graph.hpp class

---
+++ Code/graph.hpp
@@ -0,0 +1,17 @@
+#ifndef GRAPH_HPP
+#define GRAPH_HPP
+
+#include <iostream>
+
+class graph{
+private:
+    /* data */
+public:
+    graph(int*, int**);
+    void swap(int, int);
+    void youngestCommander(int);
+    void meeting();
+};
+
+
+#endif
